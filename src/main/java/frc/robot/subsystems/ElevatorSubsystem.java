1e5c955baca4ea8054ba34f0f31f52c13c4f1e72
NAHS Robotics <nahsrobotics@napls.us>
2019-05-15 19:17:16 -0400

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,181 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.commands.ElevatorCommand;
+
+/**
+ * A Linear elevator elevator subsystem
+ */
+public class ElevatorSubsystem extends Subsystem {
+
+    private double[] d_levels = { 0, .95, 1.45 };
+
+    private final double k_PULLEY_CIRCUMFERENCE = .102;
+    private final double k_GEAR_RATIO = 64;
+    private final double k_PULESES_PER_REVOLUTION = 1024;
+
+    public static enum Levels {
+        ONE, TWO, THREE;
+    }
+
+    public static enum ElevatorDirection {
+        UP, DOWN;
+    }
+
+    private PWMTalonSRX m_motor; // The elevator motor
+
+    private double m_speedUp = 1; // The motor speed when the elevator is traveling up
+    private double m_speedDown = -.7; // The motor speed when elevator is traveling down
+
+    private Levels m_level = Levels.ONE;
+
+    private Encoder encoder;
+
+    @Override
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new ElevatorCommand());
+    }
+
+    /**
+     * A linear elevator elevator subsystem
+     * 
+     * @param id         - The linear elevator motor PWM port on the roborio
+     * @param isInverted - Is the motor inverted
+     * @param speedUp    - The motor speed when the elevator is traveling up
+     * @param speedDown  - The motor speed when elevator is traveling down
+     * @param d_levels   - The height in meters in relation to encoder ticks for
+     *                   each level of the rocket.
+     * @param encoder    - The elevator encoder
+     */
+    public ElevatorSubsystem(int id, boolean isInverted, double speedUp, double speedDown, double[] d_levels,
+            Encoder encoder) {
+        this.m_motor = new PWMTalonSRX(id);
+        this.m_motor.setInverted(isInverted);
+
+        this.m_speedUp = speedUp;
+        this.m_speedDown = speedDown;
+
+        this.d_levels = d_levels;
+
+        this.encoder = encoder;
+
+    }
+
+    /**
+     * A linear elevator elevator subsystem
+     * 
+     * @param id         - The linear elevator motor PWM port on the roborio
+     * @param isInverted - Is the motor inverted
+     * 
+     **/
+    public ElevatorSubsystem(int id, boolean isInverted) {
+        this.m_motor = new PWMTalonSRX(id);
+        this.m_motor.setInverted(isInverted);
+    }
+
+    /**
+     * A Linear elevator elevator subsystem
+     * 
+     * @param id - The linear elevator motor PWM port on the roborio
+     */
+    public ElevatorSubsystem(int id) {
+        this.m_motor = new PWMTalonSRX(id);
+    }
+
+    /**
+     * Set the levels for the rocket based on height
+     * 
+     * @param d_levels - A double array where the first index is the home state and
+     *                 the last index is the third level of the rocket
+     */
+    public void setLevels(double[] levels) {
+        d_levels = levels;
+    }
+
+    /**
+     * Get the height for the level requested
+     * 
+     * @param level - The level requested
+     * @return The height in meters in relation to the encoder ticks for the
+     *         elevator
+     */
+    public double getHeightFromLevel(Levels level) {
+        switch (level) {
+        case ONE:
+            return this.d_levels[0];
+        case TWO:
+            return this.d_levels[1];
+        case THREE:
+            return this.d_levels[2];
+        default:
+            return d_levels[0];
+        }
+    }
+
+    public double getHeight(Levels level) {
+        // currentEncoderPulses / (pulesesPerRevolution * gear ratio) *
+        // circumferenceOfPulley
+        return (this.encoder.get() / (k_PULESES_PER_REVOLUTION * k_GEAR_RATIO)) * k_PULLEY_CIRCUMFERENCE;
+
+    }
+
+    /**
+     * Drive the elevator using a custom speed
+     * 
+     * @param speed - The elevator speed
+     */
+    public void driveElevatorBySpeed(double speed) {
+        this.m_motor.set(speed);
+    }
+
+    /**
+     * Dirve the elevator using direction
+     * 
+     * @param dir - The direction the elevator will be going in
+     */
+    public void driveElevatorByDirection(ElevatorDirection dir) {
+        this.m_motor.set((dir == ElevatorDirection.UP) ? m_speedUp : m_speedDown);
+    }
+
+    /**
+     * Move the elevator based on a level you are trying to reach
+     * 
+     * @param level         - The desired level
+     * @param currentHeight - The current height
+     */
+    public void driveElevatorByLevel(Levels level, double currentHeight) {
+        double targetHeight = this.getHeightFromLevel(level);
+        if (targetHeight > currentHeight) {
+            this.driveElevatorByDirection(ElevatorDirection.DOWN);
+        } else {
+            this.driveElevatorByDirection(ElevatorDirection.UP);
+        }
+    }
+
+    /**
+     * Stop the elevator motor
+     */
+    public void stop() {
+        this.m_motor.stopMotor();
+    }
+
+    /**
+     * Set the current elevator level based on rocket levels
+     * 
+     * @param level - The level the elevator is currently on
+     */
+    public void setCurrentLevel(Levels level) {
+        m_level = level;
+    }
+
+}
---
+++ src/main/java/frc/robot/utils/PIDFController.java
@@ -0,0 +1,133 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.utils;
+
+/**
+ * A custom Closed-Loop Feedback PIDF Controller
+ */
+public class PIDFController {
+
+    private String name;
+    private double kP, kI, kD, kF, setpoint;
+
+    /**
+     * The PID Controller constructor
+     * 
+     * @param name - The Subsytem name
+     * @param kP   - the Proportional gain
+     * @param kI   - the Integral gain
+     * @param kD   - the Derivative gain
+     * @param kF   - the Feed Forward gain
+     */
+    public PIDFController(String name, double kP, double kI, double kD, double kF) {
+        this.name = name;
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+        this.kF = kF;
+    }
+
+    /**
+     * Create a blank PID Controller
+     * 
+     * @param name - The subsystem name
+     */
+    public PIDFController(String name) {
+        this(name, 0, 0, 0, 0);
+    }
+
+    /**
+     * A P Controller
+     * 
+     * @param name - the subsystem name
+     * @param kP   - the proportional gain
+     */
+    public PIDFController(String name, double kP) {
+        this(name, kP, 0, 0, 0);
+    }
+
+    /**
+     * A PI Controller
+     * 
+     * @param name - the subsystem name
+     * @param kP   - the proportional gain
+     * @param kI   - the integral gain
+     */
+    public PIDFController(String name, double kP, double kI) {
+        this(name, kP, kI, 0, 0);
+    }
+
+    /**
+     * A PID Controller
+     * 
+     * @param name - the subsystem name
+     * @param kP   - the proportional gain
+     * @param kI   - the integral gain
+     * @param kD   - the derivative gain
+     */
+    public PIDFController(String name, double kP, double kI, double kD) {
+        this(name, kP, kI, kD, 0);
+    }
+
+    /**
+     * Set the proportinal gain
+     * 
+     * @param kP - the proportional gain
+     */
+    public void setP(double kP) {
+        this.kP = kP;
+    }
+
+    /**
+     * Set the integral gain
+     * 
+     * @param kI - the integral gain
+     */
+    public void setI(double kI) {
+        this.kI = kI;
+    }
+
+    /**
+     * Set the derivative gain
+     * 
+     * @param kD - the derivative gain
+     */
+    public void setD(double kD) {
+        this.kD = kD;
+    }
+
+    /**
+     * Set the feed forward gain
+     * 
+     * @param kF - the feed forward gain
+     */
+    public void setF(double kF) {
+        this.kF = kF;
+    }
+
+    /**
+     * Set the setpoint (aka target)
+     * 
+     * @param setpoint - The setpoint
+     */
+    public void setSetPoint(double setpoint) {
+        this.setpoint = setpoint;
+    }
+
+    /**
+     * Calculate the output
+     * 
+     * @param input - The sensor input
+     * @return The output
+     */
+    public double calculate(double input) {
+        double output = 0;
+        return output;
+    }
+
+}
